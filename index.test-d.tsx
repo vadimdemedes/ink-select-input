5ce6d08961a2b321af893cbd43433c915be0be89
Sebastian Sebald <sebastian.sebald@reservix.de>
2019-03-25 11:08:25 +0100

    style updates

--- index.test-d.tsx
+++ index.test-d.tsx
@@ -1,15 +1,29 @@
-import * as React from 'react';
-import InkSelect, { Item } from '.';
+import * as React from "react";
+import InkSelect, { Item } from ".";
 
-const plain = () => (<InkSelect/>);
-const withProps = () => (<InkSelect focus={false} limit={10} />);
+const items: Item[] = [
+	{ label: "label 1", value: "label 1" },
+	{ label: "label 2", value: "label 2" }
+];
+const keyedItems: Item[] = [
+	{ label: "label 1", value: "label 1", key: 1 },
+	{ label: "label 2", value: "label 2", key: 2 }
+];
 
-const items = () => (<InkSelect items={[{ label: 'label 1', value: 'label 1'}, { label: 'label 2', value: 'label 2'}]}/>);
-const itemsWithKey = () => (<InkSelect items={[{ label: 'label 1', value: 'label 1', key: 1 }, { label: 'label 2', value: 'label 2', key: 2 }]}/>);
+const plain = () => <InkSelect />;
+const withProps = () => <InkSelect focus={false} limit={10} />;
 
-const onSelect = (item:Item) => console.log(item);
-const withHandler = () => (<InkSelect onSelect={onSelect}/>);
+const selectWithItems = () => <InkSelect items={items} />;
+const selectWithKeyedItems = () => <InkSelect items={keyedItems} />;
 
-const CustomIndikator:React.FC = () => (<div/>);
-const CustomItemComponent:React.FC = () => null;
-const overrideComponents = () => (<InkSelect indicatorComponent={CustomIndikator} itemComponent={CustomItemComponent}/>);
+const onSelect = (item: Item) => console.log(item);
+const withHandler = () => <InkSelect onSelect={onSelect} />;
+
+const CustomIndikator: React.FC = () => <div />;
+const CustomItemComponent: React.FC = () => null;
+const overrideComponents = () => (
+	<InkSelect
+		indicatorComponent={CustomIndikator}
+		itemComponent={CustomItemComponent}
+	/>
+);
