45def6807d7c6c78d7c3e1eb52c778db7ed22aab
Sebastian Sebald <sebastian.sebald@gmail.com>
2019-03-28 03:27:48 +0100

    Add TS typings (#17)
    * Add TS typings
    
    * Add docs
    
    * Add typechecking script to tests
    
    * style updates
    
    * Add typings for custom indicator and item component
    
    * update formating
    
    * Update index.d.ts

---
+++ index.test-d.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import InkSelect, {Item, IndicatorProps, ItemProps} from '.';
+
+const items: Item[] = [
+	{label: 'label 1', value: 'label 1'},
+	{label: 'label 2', value: 'label 2'}
+];
+const keyedItems: Item[] = [
+	{label: 'label 1', value: 'label 1', key: 1},
+	{label: 'label 2', value: 'label 2', key: 2}
+];
+
+const plain = () => <InkSelect />;
+const withProps = () => <InkSelect focus={false} limit={10} />;
+
+const selectWithItems = () => <InkSelect items={items} />;
+const selectWithKeyedItems = () => <InkSelect items={keyedItems} />;
+
+const onSelect = (item: Item) => console.log(item);
+const withHandler = () => <InkSelect onSelect={onSelect} />;
+
+const CustomIndikator: React.FC<IndicatorProps> = ({isSelected}) => (
+	<div>{isSelected ? '✓' : ''}</div>
+);
+const CustomItemComponent: React.FC<ItemProps> = ({isSelected, label}) => (
+	<div>
+		{isSelected ? '✓' : ''} {label}
+	</div>
+);
+const overrideComponents = () => (
+	<InkSelect
+		indicatorComponent={CustomIndikator}
+		itemComponent={CustomItemComponent}
+	/>
+);
