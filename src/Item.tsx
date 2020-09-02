c66e64d4152590c3e55bf0414ddaecdeac0b4b1f
Yosef Herskovitz <yosefherskovitz@gmail.com>
2020-09-02 14:35:04 -0400

    added allowjs to tsconfig.json

--- src/Item.tsx
+++ src/Item.tsx
@@ -10,7 +10,7 @@ export interface Props {
 	defaultColor: LiteralUnion<typeof ForegroundColor, any>;
 	accentColor: LiteralUnion<typeof ForegroundColor, any>;
 }
-
+// typeof chalk.ForegroundColor | typeof chalk.BackgroundColor | typeof chalk.Modifiers;
 const Item: FC<Props> = ({isSelected = false, label, defaultColor, accentColor}) => (
 	<Text color={isSelected ? accentColor : {defaultColor}}>{label}</Text>
 );
